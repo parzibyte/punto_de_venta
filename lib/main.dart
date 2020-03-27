80a68967deee8d7b8a38cff48b1e2dfacbeaad09
Luis Cabrera Benito <parzibyte@gmail.com>
2020-03-27 12:20:29 -0600

    Mostrando loading en login
    Manejando correctamente lo asíncrono en el login para mostrar el loading general (cuando se comprueba si el token obtenido de las sharedpreferences es válido) y el del botón (cuando se hace login debido a que el token no está presente)

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'constantes.dart';
 import 'navigator.dart';
 
-void _revisarToken() async {
-  final prefs = await SharedPreferences.getInstance();
-  String posibleToken = prefs.getString("token_api");
-  log("Posible token: $posibleToken");
-  if (posibleToken != null) {
-    navegarAEscritorio();
-  }
-}
-
-void _guardarToken(String token) async {
+Future _guardarToken(String token) async {
   log("Estoy guardando el token...");
   final prefs = await SharedPreferences.getInstance();
   prefs.setString("token_api", token);
   log("Terminado de guardar token");
-  navegarAEscritorio();
 }
 
 void main() => runApp(MyApp());
@@ -57,15 +47,6 @@ class MyApp extends StatelessWidget {
       navigatorKey: navigatorKey,
       title: 'Punto de venta',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Inicio de sesión'),
@@ -75,16 +56,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -92,104 +63,132 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<bool> login(String usuario, String password) async {
+    setState(() {
+      cargandoBoton = true;
+    });
+
+    final http.Response response = await http.post(
+      rutaLogin,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': usuario,
+        'password': password,
+      }),
+    );
+    setState(() {
+      this.cargandoBoton = false;
+    });
+    if (response.statusCode == 200) {
+      Map<String, dynamic> respuesta = json.decode(response.body);
+      var token = respuesta["access_token"];
+      await _guardarToken(token);
+      return true;
+    }
+    return false;
+  }
+
+  Future<bool> _revisarSiTokenEsValido() async {
+    setState(() {
+      this.cargandoGeneralmente = true;
+    });
+    final prefs = await SharedPreferences.getInstance();
+    String posibleToken = prefs.getString("token_api");
+    if (posibleToken == null) {
+      return false;
+    }
+    final http.Response response =
+        await http.get('$RUTA_API/user', headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $posibleToken',
+    });
+    setState(() {
+      this.cargandoGeneralmente = false;
+    });
+
+    return response.statusCode == 200;
+  }
+
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
-  Future<String> _futureLogin;
+  bool cargandoGeneralmente = false;
+  bool cargandoBoton = false;
 
   @override
   void initState() {
     super.initState();
-    _revisarToken();
+    this.navegarAEscritorioSiEsPosible();
+  }
+
+  void navegarAEscritorioSiEsPosible() async {
+    var respuesta = await _revisarSiTokenEsValido();
+    if (respuesta) navegarAEscritorio();
+    log("La respuesta es:" + respuesta.toString());
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            (_futureLogin != null)
-                ? FutureBuilder<String>(
-                    future: _futureLogin,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Text("Bienvenido");
-                      } else if (snapshot.hasError) {
-                        return Text("Error iniciando sesión");
-                      }
-                      return CircularProgressIndicator();
-                    },
-                  )
-                : Text(""),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _email,
-                decoration: InputDecoration(
-                    hintText: 'correo@dominio',
-                    labelText: "Correo electrónico"),
-                keyboardType: TextInputType.emailAddress,
+        child: cargandoGeneralmente
+            ? CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _email,
+                      decoration: InputDecoration(
+                          hintText: 'correo@dominio',
+                          labelText: "Correo electrónico"),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _password,
+                      decoration: InputDecoration(
+                          hintText: 'Contraseña', labelText: 'Contraseña'),
+                      obscureText: true, /* <-- Aquí */
+                    ),
+                  ),
+                  Builder(
+                    builder: (context) => RaisedButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      child: cargandoBoton
+                          ? CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                            )
+                          : Text('Iniciar sesión'),
+                      onPressed: () async {
+                        bool respuesta =
+                            await login(_email.text, _password.text);
+                        if (respuesta) {
+                          navegarAEscritorio();
+                        } else {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content:
+                                  Text('Datos incorrectos. Intenta de nuevo'),
+                              duration: Duration(seconds: 1),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _password,
-                decoration: InputDecoration(
-                    hintText: 'Contraseña', labelText: 'Contraseña'),
-                obscureText: true, /* <-- Aquí */
-              ),
-            ),
-            RaisedButton(
-              child: Text('Iniciar sesión'),
-              onPressed: () {
-//                var xd = await hacerLogin("", "");
-//                Navigator.push(
-//                  context,
-//                  MaterialPageRoute(builder: (context) => Escritorio()),
-//                );
-                setState(() {
-                  _futureLogin = hacerLogin(_email.text, _password.text);
-                });
-//                  final scaffold = Scaffold.of(context);
-//                  scaffold.showSnackBar(
-//                    SnackBar(
-//                      content: const Text('Ejemplo'),
-//                    ),
-//                  );
-              },
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
